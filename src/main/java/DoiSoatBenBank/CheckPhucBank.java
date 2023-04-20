07d6654deeadf11146a840dd7fa6b418e87f1b7b
ducha <ducha@lienviettech.com.vn>
2023-04-20 15:49:55 +0700

    FIX BUG

--- src/main/java/DoiSoatBenBank/CheckPhucBank.java
+++ src/main/java/DoiSoatBenBank/CheckPhucBank.java
@@ -17,31 +17,62 @@ public class CheckPhucBank {
 
     private static String toDate = "2023-04-17";
 
-    private static String linkFileLvbis = "C:\\Users\\saotr\\Downloads\\Viettel\\LVBIT\\";
+    private static String linkFileLvbis = "D:\\DucHA\\LVBIT\\";
 
-    private static String linkFileViettel = "C:\\Users\\saotr\\Downloads\\Viettel\\Viettel\\";
+    private static String linkFileViettel = "D:\\DucHA\\ViettelFile\\";
 
-    private static String linkFileLvt = "";
+    private static String linkFileLvt = "D:\\DucHA\\LV24H\\new 2.tsv";
 
     private static  void checkLvt() throws IOException {
-        //read file Phúc gửi, xử lý từng dòng
-        //Lấy PAYMENT_REF_NO(TRANSACTION_ID), check trong Map{TRANSACTION_ID,TrạngThái} có không, có thì trả về trạng thái, không thì trả về Empty
-        //Ghi file kết quả
 
+        List<LvbEntity> listLvbis = getDataLvBis(); //Get file Lvbis
+        List<ViettelEntity> listViettel = getDataViettels(); //Get file viettel
+        Map<String,BankEntity> mapLVT = getDataLvt();  //Get File LV24h;
 
+        System.out.println("TRANS_ID" + "\t" + "USER_NAME" + "\t" + "FULL_NAME" + "\t" + "TRANS_TIME"
+                + "\t" + "TRANS_NAME" + "\t" + "INPUT_VALUE" + "\t" + "AMOUNT_TRANSFER" + "\t" + "INPUT_AMOUNT"
+                + "\t" + "PAYMENT_REF_NO" + "\t" + "PARTNER_CODE" + "\t" + "FROM_SYSTEM_ID"  + "\t" + "STATUS");
 
-        List<LvbEntity> listLvbis = getDataLvBis();
-        List<ViettelEntity> listViettel = getDataViettels();
-
-        Map<String,BankEntity> mapLVT = getDataLvt();
-
-        String tranId = "230413030007000156";
-//        String tranId = "230415030007000025";
-
-        String status = compareLvbWithVietetl(listLvbis,listViettel,tranId);
-        if(status!=null){
-            mapLVT.get(tranId)
+        Set<String> lvtTransIds = mapLVT.keySet();
+        for (String item:lvtTransIds) {
+            String status = compareLvbWithVietetl(listLvbis,listViettel,item);
+            if(status==null){
+                BankEntity bankEntity = mapLVT.get(item);
+                System.out.println(
+                        bankEntity.getTRANS_ID() + "\t"
+                                + bankEntity.getUSER_NAME() + "\t"
+                                + bankEntity.getFULL_NAME() + "\t"
+                                + bankEntity.getTRANS_TIME() + "\t"
+                                + bankEntity.getTRANS_NAME() + "\t"
+                                + bankEntity.getINPUT_VALUE() + "\t"
+                                + bankEntity.getAMOUNT_TRANSFER()+ "\t"
+                                + bankEntity.getINPUT_AMOUNT() + "\t"
+                                + bankEntity.getPAYMENT_REF_NO() + "\t"
+                                + bankEntity.getPARTNER_CODE() + "\t"
+                                + bankEntity.getFROM_SYSTEM_ID()+ "\t"
+                                + "Thất bại"
+                );
+            }else {
+                //Có bên file viettel
+                BankEntity bankEntity = mapLVT.get(item);
+                System.out.println(
+                        bankEntity.getTRANS_ID() + "\t"
+                                + bankEntity.getUSER_NAME() + "\t"
+                                + bankEntity.getFULL_NAME() + "\t"
+                                + bankEntity.getTRANS_TIME() + "\t"
+                                + bankEntity.getTRANS_NAME() + "\t"
+                                + bankEntity.getINPUT_VALUE() + "\t"
+                                + bankEntity.getAMOUNT_TRANSFER()+ "\t"
+                                + bankEntity.getINPUT_AMOUNT() + "\t"
+                                + bankEntity.getPAYMENT_REF_NO() + "\t"
+                                + bankEntity.getPARTNER_CODE() + "\t"
+                                + bankEntity.getFROM_SYSTEM_ID()+ "\t"
+                                + status
+                );
+            }
         }
+
+
     }
 
     private static List<ViettelEntity> getDataViettels() {// Map{orderId, TrạngThái}
@@ -70,29 +101,38 @@ public class CheckPhucBank {
 
     }
 
-    public static Map<String,BankEntity> getDataLvt() throws IOException {
-        Map<String,BankEntity> mapBankCheck = new HashMap<>();
-        //Đọc file của bank cần check
-        List<String> lines4 = Files.readAllLines(Paths.get(linkFileLvt));
-        for (String line : lines4) {
-            String[] sps = line.split("\t");
-                String transactionId = sps[8];
-                BankEntity bankEntity = new BankEntity();
-                bankEntity.setTRANS_ID(sps[0]);
-                bankEntity.setUSER_NAME(sps[1]);
-                bankEntity.setFULL_NAME(sps[2]);
-                bankEntity.setTRANS_TIME(sps[3]);
-                bankEntity.setTRANS_NAME(sps[4]);
-                bankEntity.setINPUT_VALUE(sps[5]);
-                bankEntity.setAMOUNT_TRANSFER(sps[6]);
-                bankEntity.setINPUT_AMOUNT(sps[7]);
-                bankEntity.setPAYMENT_REF_NO(sps[8]);
-                bankEntity.setPARTNER_CODE(sps[9]);
-                bankEntity.setFROM_SYSTEM_ID(sps[10]);
-                mapBankCheck.put(transactionId, bankEntity);
-            }
+    public static HashMap<String,BankEntity> getDataLvt() throws IOException {
+      try {
+          HashMap<String,BankEntity> mapBankCheck = new HashMap<>();
+          //Đọc file của bank cần check
+          List<String> lines4 = Files.readAllLines(Paths.get(linkFileLvt));
+          for (String line : lines4) {
+              String[] sps = line.split("\t");
+              String transactionId = sps[8];
+              if(!"PAYMENT_REF_NO".equals(transactionId)){
+                  BankEntity bankEntity = new BankEntity();
+                  bankEntity.setTRANS_ID(sps[0]);
+                  bankEntity.setUSER_NAME(sps[1]);
+                  bankEntity.setFULL_NAME(sps[2]);
+                  bankEntity.setTRANS_TIME(sps[3]);
+                  bankEntity.setTRANS_NAME(sps[4]);
+                  bankEntity.setINPUT_VALUE(sps[5]);
+                  bankEntity.setAMOUNT_TRANSFER(sps[6]);
+                  bankEntity.setINPUT_AMOUNT(sps[7]);
+                  bankEntity.setPAYMENT_REF_NO(sps[8]);
+                  bankEntity.setPARTNER_CODE(sps[9]);
+                  bankEntity.setFROM_SYSTEM_ID(sps[10]);
+                  mapBankCheck.put(transactionId, bankEntity);
+              }
 
-        return mapBankCheck;
+          }
+          return mapBankCheck;
+      }catch (NullPointerException e){
+
+      }catch (Exception e){
+          System.out.println("Lỗi đọc file LV24H");
+      }
+      return null;
 
     }
 
@@ -118,7 +158,7 @@ public class CheckPhucBank {
                         }
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e){
 
         }
         return listMapLvbis;
@@ -128,25 +168,32 @@ public class CheckPhucBank {
         String status = null;
         try{
 
-            String orderId = listLvbis.stream()
-                    .filter(item -> item.getTRANSACTION_ID() != null&&item.getTRANSACTION_ID().equals(trandstion_id))
-                    .findAny().get().getORDER_ID_REQ();
+//            String orderId = listLvbis.stream()
+//                    .filter(item -> item.getTRANSACTION_ID() != null&&item.getTRANSACTION_ID().equals(trandstion_id))
+//                    .findAny().get().getORDER_ID_REQ();
 
-                        if(orderId!=null){
-                            try {
-                                 for (ViettelEntity item:listViettel){
-                                     if(item.getORDER_ID()!=null&&orderId.equals(item.getORDER_ID())){
-                                          status = item.getTRANG_THAI();
-                                          break;
-                                     }
-                                 }
-                                 if(status==null){
-                                     status = "Thất bại";
-                                 }
-                            }catch (Exception e){
-                                System.out.println("Lỗi phát sinh khi tìm trong file viettel");
+            for (LvbEntity item:listLvbis) {
+                if(item.getTRANSACTION_ID()!=null&&item.getTRANSACTION_ID().equals(trandstion_id)){
+                    String orderId = item.getORDER_ID_REQ();
+                    if(orderId!=null){
+                        try {
+                            for (ViettelEntity item1:listViettel){
+                                if(item1.getORDER_ID()!=null&&orderId.equals(item1.getORDER_ID())){
+                                    status = item1.getTRANG_THAI();
+                                    break;
+                                }
                             }
+                            if(status==null){
+                                status = "Thất bại";
+                            }
+                        }catch (Exception e){
+                            System.out.println("Lỗi phát sinh khi tìm trong file viettel");
                         }
+                    }
+                }
+            }
+
+
 
         }catch (NullPointerException e){
             System.out.println(e.getMessage());
@@ -159,7 +206,6 @@ public class CheckPhucBank {
     private static File[] getListFile(String toDate, int rangerDate, String linkFile, String formatFile) {
         File[] fileArray = new File[rangerDate];
         try {
-
             for (int i = 0; i < rangerDate; i++) {
                 StringBuilder link = new StringBuilder(linkFile);
                 link.append(dateCompare(toDate, i)).append(formatFile);
@@ -184,11 +230,11 @@ public class CheckPhucBank {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String tranId = "230413030007000156";
 //        String tranId = "230415030007000025";
-        String status = checkLvt(tranId);
-
+//        String status = checkLvt(tranId);
+          checkLvt();
         }
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import DoiSoatBenBank.LvbEntity;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -13,63 +15,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
-//        link.append("text1.txt");
-//
-//        StringBuilder link2 = new StringBuilder("D:\\DucHA\\LVBIT\\");
-//        link2.append("text2.txt");
-
-        String toDate = "2023-04-19";
-
-//        File[] files = {new File(String.valueOf(link)), new File(String.valueOf(link2))};
-        File[] fileArray = new File[3];
-        try {
-
-            for (int i = 0; i < 3; i++) {
-                StringBuilder link = new StringBuilder("D:\\DucHA\\LVBIT\\");
-                link.append(dateCompare(toDate,i)).append(".txt");
-                fileArray[i] = new File(String.valueOf(link));
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Lỗi không thể mở list file Lvbis");
-        }
-
-        System.out.println();
-        // Fetching all the files
-        for (File file : fileArray) {
-            if (file.isFile()) {
-                BufferedReader inputStream = null;
-                String line;
-                try {
-                    inputStream = new BufferedReader(new FileReader(file));
-                    while ((line = inputStream.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                } catch (IOException e) {
-                    System.out.println(e);
-                } finally {
-                    if (inputStream != null) {
-                        inputStream.close();
-                    }
-                }
-            }
-        }
-
-
     }
-
-    public static String dateCompare(String toDate, int rangerDate) throws ParseException {
-        String datetimeFormatPattern = "yyyy-MM-dd";
-        Date date = new SimpleDateFormat(datetimeFormatPattern).parse(toDate);
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.add(Calendar.DATE, - rangerDate); //minus number would decrement the days
-        cal.getTime();
-        String dateString = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime());
-        return dateString;
-    }
-
-
 }
 
